f68470c5858f2021ebcf7651f0f559c9f8b4960a
Your Name <you@example.com>
2017-05-10 14:10:23 -0300

    ..

---
+++ src/components/decks/DeckPartidas.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <div v-for="partida in partidas">
+    {{ partida.evento }} :
+    <deck-cores :cores="partida.matchup.cores"></deck-cores>
+    <deck-arquetipos :arquetipos="partida.matchup.arquetipos"></deck-arquetipos>
+    <deck-tipos :tipos="partida.matchup.tipos"></deck-tipos>
+  </div>
+</div>
+</template>
+
+<script>
+import {
+  DeckCores,
+  DeckArquetipos,
+  DeckTipos
+} from '@/components/decks'
+
+export default {
+  name: 'deck-partidas',
+  props: ['partidas'],
+  components: {
+    DeckCores,
+    DeckArquetipos,
+    DeckTipos
+  }
+}
+</script>
+
+<style scoped>
+</style>
