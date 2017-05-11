655a37a1082c7a0b7322f19cfe115a05ba9da8bc
robsontenorio <robsontenorio@me.com>
2017-05-10 23:00:17 -0300

    ..

---
+++ src/components/estatisticas/index.js
@@ -0,0 +1,11 @@
+import EstatisticaGeral from './EstatisticaGeral'
+import EstatisticaFaerias from './EstatisticaFaerias'
+import EstatisticaProgresso from './EstatisticaProgresso'
+import EstatisticaTerrenos from './EstatisticaTerrenos'
+
+export {
+  EstatisticaGeral,
+  EstatisticaFaerias,
+  EstatisticaProgresso,
+  EstatisticaTerrenos
+}
