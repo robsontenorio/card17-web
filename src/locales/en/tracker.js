60afdad7f8c03956aa29387e642ce9c043ac10bc
robsontenorio <robsontenorio@me.com>
2017-08-02 00:09:47 -0300

    textos da versao 1.0.3

--- src/locales/en/tracker.js
+++ src/locales/en/tracker.js
@@ -7,7 +7,7 @@ const messages = {
   experimental: 'This is experimental',
   experimental_info: 'As we have said, DECKTRACKER looks for network communication patterns while playing FAERIA. In this way, if the FAERIA is rebuilt or the pattern is modified, DECKTRACKER will not work. Also, newly released cards will not be listed. Well, we need some Yaks to help update DECKTRACKER frequently. You can collaborate by reporting bugs on the Discord Channel <a href="https://discord.gg/R5g3PwW"> #card17 </a>',
   problemas: 'Known issues',
-  problemas_info: '- Does not work on PANDORA MODE.<BR><BR>- Cards that creates RANDOM cards can mess up the DECKTRACKER counting when they are played (eg, Spellwhirl).<br><br>- If the connection is lost or the game is closed, restart the procedure: Close the FAERIA, close the TRACKER. Open the TRACKER, open the FAIRY. Of course, you can finish your match first before closing!<br><br>- Decks created during the session are not recognized immediately by the DECKTRACKER (you must restart the procedure). Edited decks are recognized normally.<br><br>- Works locally and does not send data to CARD17 ... for now.',
+  problemas_info: '- Does not work on PANDORA MODE.<br><br>- If the connection is lost or the game is closed abruptly, DECKTRACKER will begin tracking in the next match.<br><br>- Decks created during the session are not recognized immediately by the DECKTRACKER (you must restart FAERIA). Edited decks are recognized normally.<br><br>- Works locally and does not send data to CARD17 ... for now.',
   entendi: 'Ok, I got it ...',
   nota: 'note',
   nota_win: 'You should install <a href="http://www.card17.com/WinPcap.zip"> WinPcap<br> </a>before</b> installing DECKTRACKER',
